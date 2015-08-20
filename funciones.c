bdb193db09c6b947200a772f5e7d191cef3656ae
Luisa95 <luisaflopez33@gmail.com>
2015-08-20 12:11:00 -0500

    funciones.c

---
+++ funciones.c
@@ -0,0 +1,15 @@
+#include "funciones.h"
+
+void MostrarRegistro(int registro[],int pos)
+{
+    int i;
+
+    for(i=0;i<=11;i++)
+    {
+       printf("\n\tposicion del registro r%d:%d",i,registro[i]);
+    }
+
+    printf("\n\tLa posicion %d del registro contiene:%d",pos,registro[pos]);
+
+ return 0;
+}
