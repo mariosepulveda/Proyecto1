30eb1cfad94c4d3318bb0cb88372c0f67eb4214f
marioseulveda <masepulvedar@uqvirtual.edu.co>
2015-08-27 12:19:00 -0500

    Se modifico el archivo alu.h

--- Proyecto1/alu.c
+++ Proyecto1/alu.c
@@ -4,21 +4,25 @@
 void ADD(int8_t Rd,int8_t Rr)
 {
     Rd=Rd+Rr;//suma 2 registros
+    return Rd;
 };
 
 void SUB(int8_t Rd,int8_t Rr)
 {
     Rd=Rd-Rr;//resta 2 registros
+    return Rd;
 };
 
 void INC(int8_t Rd)
 {
     Rd=Rd+1;//incrementa el registro
+    return Rd;
 };
 
 void DEC(int8_t Rd)
 {
     Rd=Rd-1;//decrementa el registro
+    return Rd;
 };
 
 void AND(int8_t Rd,int8_t Rr)
