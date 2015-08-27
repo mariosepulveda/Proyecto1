5cee77f803a49103e813a11d71cb5880d5f121a4
marioseulveda <masepulvedar@uqvirtual.edu.co>
2015-08-27 12:30:12 -0500

    se modifico la alu

--- Proyecto1/alu.c
+++ Proyecto1/alu.c
@@ -1,46 +1,50 @@
 
 #include "alu.h"
 
-void ADD(int8_t Rd,int8_t Rr)
+int8_t ADD(int8_t Rd,int8_t Rr)
 {
     Rd=Rd+Rr;//suma 2 registros
     return Rd;
 };
 
-void SUB(int8_t Rd,int8_t Rr)
+int8_t SUB(int8_t Rd,int8_t Rr)
 {
     Rd=Rd-Rr;//resta 2 registros
     return Rd;
 };
 
-void INC(int8_t Rd)
+int8_t INC(int8_t Rd)
 {
     Rd=Rd+1;//incrementa el registro
     return Rd;
 };
 
-void DEC(int8_t Rd)
+int8_t DEC(int8_t Rd)
 {
     Rd=Rd-1;//decrementa el registro
     return Rd;
 };
 
-void AND(int8_t Rd,int8_t Rr)
+int8_t AND(int8_t Rd,int8_t Rr)
 {
     Rd=Rd&Rr;//and entre los registros
+    return Rd;
 };
 
-void OR(int8_t Rd,int8_t Rr)
+int8_t OR(int8_t Rd,int8_t Rr)
 {
     Rd=Rd|Rr;//or entre los registros
+    return Rd;
 };
 
-void EOR(int8_t Rd,int8_t Rr)
+int8_t EOR(int8_t Rd,int8_t Rr)
 {
     Rd=Rd^Rr;//xor entre los registros
+    return Rd;
 };
 
-void MOV(int8_t Rd, int8_t Rr)
+int8_t MOV(int8_t Rd, int8_t Rr)
 {
     Rd=Rr;
+    return Rd;
 };
--- Proyecto1/alu.h
+++ Proyecto1/alu.h
@@ -2,28 +2,28 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-void ADD(int8_t Rd,int8_t Rr);
+int8_t ADD(int8_t Rd,int8_t Rr);
 /**
 *\brief funcion que suma
 *\param Rd operando 1
-*\param Rr operando 2 
+*\param Rr operando 2
 *\return un entero con el resultado
 **/
-void SUB(int8_t Rd,int8_t Rr);
+int8_t SUB(int8_t Rd,int8_t Rr);
 /**
 *\brief funcion que resta
 *\param Rd operando 1
-*\param Rr operando 2 
+*\param Rr operando 2
 *\return un entero con el resultado
 **/
-void AND(int8_t Rd,int8_t Rr);
+int8_t AND(int8_t Rd,int8_t Rr);
 /**
 *\brief funcion que resta
 *\param Rd operando 1
-*\param Rr operando 2 
+*\param Rr operando 2
 *\return un entero con el resultado
 **/
-void OR(int8_t Rd,int8_t Rr);
-void EOR(int8_t Rd,int8_t Rr);
-void MOV(int8_t Rd, int8_t Rr);
+int8_t OR(int8_t Rd,int8_t Rr);
+int8_t EOR(int8_t Rd,int8_t Rr);
+int8_t MOV(int8_t Rd, int8_t Rr);
 
